24a6518ebd0e583763cac617e7045aecc5166ec8
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-19 18:50:54 -0400

    Create basic git config file

--- pyt/init.py
+++ pyt/init.py
@@ -1,9 +1,22 @@
+import configparser
 from pathlib import Path
 from typing import Optional
 
 import typer
 
 app = typer.Typer()
+
+
+def generate_config(git_dir: Path) -> None:
+    config = configparser.ConfigParser()
+    config["core"] = {
+        "repositoryformatversion": 0,
+        "filemode": True,
+        "bare": False,
+        "logallrefupdates": True,
+    }
+    with (git_dir / "config").open("w", encoding="utf-8") as gitconfig:
+        config.write(gitconfig)
 
 
 @app.callback(invoke_without_command=True)
@@ -17,6 +30,7 @@ def init(directory: Optional[str] = typer.Argument(None)):
     try:
         for dir in layout:
             (git_dir / dir).mkdir(parents=True, exist_ok=False)
+        generate_config(git_dir)
         message = typer.style(
             f"Initialized empty Pyt repository in {typer.style(git_dir, underline=True)}",
             fg=typer.colors.GREEN,
