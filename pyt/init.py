b9af3c0f434553575b122e4aeba545d29a03ef8f
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-20 06:53:21 -0400

    Use custom dataclass instead of configparser

--- pyt/init.py
+++ pyt/init.py
@@ -1,4 +1,4 @@
-import configparser
+from dataclasses import asdict, dataclass
 from pathlib import Path
 from typing import Optional
 
@@ -7,16 +7,38 @@ import typer
 app = typer.Typer()
 
 
+@dataclass
+class GitConfigSegment:
+    """Custom dataclass provides more control over configparser module"""
+
+    segment_name: str = ""
+
+    def __post_init__(self) -> None:
+        """If a segment name is provided, prepend with a space"""
+        if self.segment_name:
+            self.segment_name = f' "{self.segment_name}"'
+
+    def __str__(self) -> str:
+        """Return formatted segment, all in lowercase"""
+        header = f"[{self.__class__.__name__}{self.segment_name}]"
+        fields = asdict(self)
+        del fields["segment_name"]
+        body = "\n".join(f"\t{name} = {value}" for name, value in fields.items())
+        return f"{header}\n{body}".lower()
+
+
+@dataclass
+class Core(GitConfigSegment):
+    repositoryformatversion: int = 0
+    filemode: bool = True
+    bare: bool = False
+    logallrefupdates: bool = True
+
+
 def generate_config(git_dir: Path) -> None:
-    config = configparser.ConfigParser()
-    config["core"] = {
-        "repositoryformatversion": 0,
-        "filemode": True,
-        "bare": False,
-        "logallrefupdates": True,
-    }
+    config = Core()
     with (git_dir / "config").open("w", encoding="utf-8") as gitconfig:
-        config.write(gitconfig)
+        gitconfig.write(str(config))
 
 
 @app.callback(invoke_without_command=True)
